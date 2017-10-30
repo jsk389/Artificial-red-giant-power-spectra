7b2a96eb134dd8434e335b99299ad7d2babff193
jsk389 <kuszlewicz@mps.mpg.de>
2017-10-30 15:27:14 +0100

    Adding to docs

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../Code/Artificial_Timeseries_PS/'))
+sys.path.insert(0, os.path.abspath('../Code/'))
 import sphinx_rtd_theme
 
 # -- General configuration ------------------------------------------------
